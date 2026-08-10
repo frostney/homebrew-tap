77edd651eb611220680f4ea187953f16317d0525
Johannes Stein <johannesstein@freeze-dev.com>
2026-08-10 12:12:34 +0100

    Update GocciaScript to 0.12.0 (#7)

--- Formula/gocciascript.rb
+++ Formula/gocciascript.rb
@@ -5,25 +5,25 @@ class Gocciascript < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/frostney/GocciaScript/releases/download/0.11.0/gocciascript-0.11.0-macos-arm64.zip"
-      sha256 "a566f62be536b24350ed4e8b089ba8946f75134e66ba1900641b1a70d3f8633f"
+      url "https://github.com/frostney/GocciaScript/releases/download/0.12.0/gocciascript-0.12.0-macos-arm64.zip"
+      sha256 "8172cbd5f355ea79dd7721493fe8074870a4f2435c7af4464562711cc08ec797"
     end
 
     on_intel do
-      url "https://github.com/frostney/GocciaScript/releases/download/0.11.0/gocciascript-0.11.0-macos-x64.zip"
-      sha256 "14826916c1ea6fbddb1cd9365b2b5846ff629172e2b09099949e64beda273090"
+      url "https://github.com/frostney/GocciaScript/releases/download/0.12.0/gocciascript-0.12.0-macos-x64.zip"
+      sha256 "01b64c8f14642f20c5c2647b768d2d78310b840232e4c18d273dfc5717f4b919"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/frostney/GocciaScript/releases/download/0.11.0/gocciascript-0.11.0-linux-arm64.tar.gz"
-      sha256 "f7c0788ab70efa8123a5be7d1b267fe555a97753a3de8ffaf0279bcdd51b9e82"
+      url "https://github.com/frostney/GocciaScript/releases/download/0.12.0/gocciascript-0.12.0-linux-arm64.tar.gz"
+      sha256 "9c9e36f40822e0abc31c54a024bb50a3deb927678370cb9b2d92dccc31d45d00"
     end
 
     on_intel do
-      url "https://github.com/frostney/GocciaScript/releases/download/0.11.0/gocciascript-0.11.0-linux-x64.tar.gz"
-      sha256 "fab93027d3598e33c218a7f20f0903051110dca5968b790e414440c86b9cf934"
+      url "https://github.com/frostney/GocciaScript/releases/download/0.12.0/gocciascript-0.12.0-linux-x64.tar.gz"
+      sha256 "48896992ac759c6ff7ef817c397b37b9abe7d7de0789d78e9f66549b6857482d"
     end
   end
 
